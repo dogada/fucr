2973523871b036ec569569bd8c71fcdd0bf933cc
D.V.D <entropyhacker@gmail.com>
2021-07-18 01:46:54 +0300

    feat: add channel section

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -4,44 +4,80 @@ import { MessageForm } from '~/channel/forms/MessageForm'
 import { loremIpsum, name, username, Avatar } from 'react-lorem-ipsum'
 import Layout from '~/ui/Layout'
 import { useMe } from '~/store'
+import Button from 'react-bootstrap/Button'
+import clsx from 'clsx'
 
 export const HomePage: NextPage = () => {
   const { me } = useMe()
   return (
     <Layout title="FUCR">
       <div className="jumbotron">
-        <h1>FUCR</h1>
-        <h3>FUll Copy Right</h3>
+        <h1>FUCR — FUll Copy Right</h1>
+        {!me && (
+          <p className="lead">
+            A digital publishing system where anyone can create and copy
+            anything. When you like something, you get a copy of it, which is
+            stored on your smartphone or computer. We do not own or control your
+            data. Your data belongs to you. We simply provide a way to
+            distribute your data to technologically and politically independent
+            data providers in order to minimize the risk of censorship of any
+            kind.
+          </p>
+        )}
+        {me && (
+          <h4 className="mt-4">
+            Welcome, {me.name}. Bellow is your unblockable data channel.
+          </h4>
+        )}
       </div>
-      <div>
-        {!me && <Setup />}
-        <label>
-          <h5>New message</h5>
-        </label>
-        <MessageForm type="message" onFinish={() => null} />
-        <div className="text-wrapper">
-          {loremIpsum({ p: 3 }).map((text) => (
-            <div className="mb-3" key={text}>
-              <Avatar
-                gender="male"
-                className="mr-2"
-                width="32"
-                height="32"
-                alt={'Name'}
-              />
-              <span>{name('male')}</span> @<strong>{username()}</strong>
-              <br />
-              {text}
-            </div>
-          ))}
-        </div>
-      </div>
+      {me ? <Channel /> : <Setup />}
     </Layout>
   )
 }
 
 function Setup() {
-  return <div>Looks like you are new here. Please create account or login.</div>
+  return (
+    <div>
+      <p>Looks like you are new here.</p>
+      <p>
+        Please
+        <Button href="/auth/create" className="mx-2">
+          Create new account
+        </Button>{' '}
+        or
+        <Button href="/auth/import" className="mx-2">
+          Import existing account
+        </Button>
+      </p>
+    </div>
+  )
+}
+
+function Channel() {
+  return (
+    <div>
+      <label>
+        <h5>New message</h5>
+      </label>
+      <MessageForm type="message" onFinish={() => null} />
+      <div className="mt-1">
+        {loremIpsum({ p: 3 }).map((text) => (
+          <div className="mb-3" key={text}>
+            <Avatar
+              gender="male"
+              className="mr-2"
+              width="32"
+              height="32"
+              alt={'Name'}
+            />
+            <span>{name('male')}</span> @<strong>{username()}</strong>
+            <br />
+            {text}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default HomePage
