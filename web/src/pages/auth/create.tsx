6e7163c02fc7cea8b515d3ad625b3139c9ea22da
D.V.D <entropyhacker@gmail.com>
2021-07-18 01:24:27 +0300

    feat: NewAccountForm

--- web/src/pages/auth/create.tsx
+++ web/src/pages/auth/create.tsx
@@ -1,14 +1,28 @@
 import * as React from 'react'
 import Layout from '~/ui/Layout'
 import Button from 'react-bootstrap/Button'
+import { NewAccountForm } from '~/auth/forms/NewAccountForm'
+import { useRouter } from 'next/router'
+import { useMe } from '~/store'
 
-const LoginPage: React.FunctionComponent = () => (
-  <Layout title="Create account">
-    <h1>Create new user account</h1>
+const LoginPage: React.FunctionComponent = () => {
+  const router = useRouter()
+  const { dispatch } = useMe()
+  function login(user) {
+    if (!user) return
+    // TODO: prompt user to export account first
+    dispatch('me/init')
+    router.push('/')
+  }
 
-    <p>If you have user account created early, you can import it.</p>
-    <Button href="/auth/import">Import account</Button>
-  </Layout>
-)
+  return (
+    <Layout title="Create account">
+      <h1>Create new user account</h1>
+      <NewAccountForm onFinish={login} />
+      <p>If you have user account created early, you can import it.</p>
+      <Button href="/auth/import">Import account</Button>
+    </Layout>
+  )
+}
 
 export default LoginPage
