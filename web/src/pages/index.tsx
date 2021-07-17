7d777d6cd2c2a26b7c8032735c5df678484fcf2f
D.V.D <entropyhacker@gmail.com>
2021-07-17 19:25:40 +0300

    feat: add lorem ipsum posts

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,23 +1,38 @@
 import { NextPage } from 'next'
 import * as React from 'react'
 import { MessageForm } from '~/channel/forms/MessageForm'
-import Layout from '../ui/Layout'
+import { loremIpsum, name, username, Avatar } from 'react-lorem-ipsum'
+import Layout from '~/ui/Layout'
 
-export const HomePage: NextPage = () => {
-  return (
-    <Layout title="FUCR">
-      <div className="jumbotron">
-        <h1>FUCR</h1>
-        <h3>FUll Copy Right</h3>
+export const HomePage: NextPage = () => (
+  <Layout title="FUCR">
+    <div className="jumbotron">
+      <h1>FUCR</h1>
+      <h3>FUll Copy Right</h3>
+    </div>
+    <div>
+      <label>
+        <h5>New message</h5>
+      </label>
+      <MessageForm type="message" onFinish={() => null} />
+      <div className="text-wrapper">
+        {loremIpsum({ p: 3 }).map((text) => (
+          <div className="mb-3" key={text}>
+            <Avatar
+              gender="male"
+              className="mr-2"
+              width="32"
+              height="32"
+              alt={'Name'}
+            />
+            <span>{name('male')}</span> @<strong>{username()}</strong>
+            <br />
+            {text}
+          </div>
+        ))}
       </div>
-      <div>
-        <label>
-          <h5>New message</h5>
-        </label>
-        <MessageForm type="message" onFinish={() => null} />
-      </div>
-    </Layout>
-  )
-}
+    </div>
+  </Layout>
+)
 
 export default HomePage
