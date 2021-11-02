1462d4d32baf051fe3b141d2a7ddc0ffb772d335
D.V.D <entropyhacker@gmail.com>
2021-11-02 21:04:35 +0200

    feat: add ipfs client support

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import { AppProps } from 'next/app'
+import { create } from 'ipfs-http-client'
+
 import { StoreonProvider } from '~/store'
 import Root from '~/ui/Root'
 import '../styles/light.scss'
 
 function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
   console.log('MyApp.init')
+  initIpfs()
   return (
     <StoreonProvider>
       <Root>
@@ -12,6 +15,24 @@ function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
       </Root>
     </StoreonProvider>
   )
+}
+async function initIpfs() {
+  console.log('initIpfs')
+  const url = new URL(process.env.NEXT_PUBLIC_IPFS_ENDPOINT as string)
+  try {
+    const ipfs = create({
+      host: url.host,
+      port: parseInt(url.port),
+      protocol: url.protocol,
+      headers: {
+        authorization: process.env.NEXT_PUBLIC_IPFS_AUTH as string
+      }
+    })
+    const res = await ipfs.add('Hello, world!')
+    console.log('res', res)
+  } catch (e) {
+    console.error('initIpfs', e)
+  }
 }
 
 export default MyApp
